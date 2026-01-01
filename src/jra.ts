34b24e95f5a13ab398e39ff16fc35e82b5c67c8f
Brad Brown <bradthomasbrown@outlook.com>
2026-01-01 03:47:57 -0600

    error handling apparently not so trivial

--- src/jra.ts
+++ src/jra.ts
@@ -9,7 +9,18 @@ class Client {
     async request(method:string, params:any):Promise<string>
     async request(...args:any):Promise<Array<string>>
     async request(...args:any):Promise<string|Array<string>> {
-        return Client.request(this.url, ...args);
+        const error = new Error();
+        error.stack = error.stack!.split('\n').toSpliced(0, 2).join('\n');
+        const result = await Client.request(this.url, ...args).catch(e => e);
+        if (result instanceof AggregateError) {
+            result.stack! += "\n" + error.stack;
+            for (let i = 0; i < result.errors.length; i++) result.errors[i].stack += "\n" + error.stack;
+            throw result;
+        } else if (result instanceof Error) {
+            result.stack! += "\n" + error.stack;
+            throw result;
+        }
+        return result;
     }
 
     static async request(url:string, method:string, params:any):Promise<string>
@@ -17,61 +28,79 @@ class Client {
     static async request(url:string, ...args:any):Promise<string|Array<string>> {
         const requests:Array<{ method:string, params:Array<any> }> = [];
         if (args.length == 2) {
-            push(requests, args[0], args[1]);
-            const [value] = await send(url, requests)
+            requests.push({ method:args[0], params:args[1] });
+            const [value] = await send(url, ...requests);
             return value;
         } else {
-            for (let i = 0; i < args.length >> 1; i++)
-                push(requests, args[i << 1], args[2 * i + 1]);
-            return send(url, requests);
+            for (let i = 0; i < args.length; i+=2) requests.push({ method:args[i], params:args[i + 1] });
+            const values = await send(url, ...requests);
+            return values;
         }
     }
 
 }
 
-function push(requests:Array<any>, method:string, params:any) {
-    requests.push({ method, params });
+// recursive value replacer
+// this should be in some other library i think
+function _fa5f71_(_02_:any, replacer:(value:any)=>any) {
+    if (typeof _02_ == "object") {
+        const _7e_:any = {};
+        for (const [key, value] of Object.entries(_02_)) {
+            if (typeof value === "object") _7e_[key] = _fa5f71_(value, replacer);
+            else _7e_[key] = replacer(value);
+        }
+        return _7e_;
+    } else {
+        return replacer(_02_);
+    }
 }
 
-function _4e4030_(errors:Array<Error>, response:any) {
-    if (!("error" in response)) return;
-    const { code, message, data } = response.error
-    const error = new Error(`${code}: ${message}`)
-    if (data) error.cause = data
-    errors.push(error);
+// send single request
+async function sendSingleRequest(url:string, requestInit:RequestInit, request:{ method:string, params:any }) {
+    requestInit.body = JSON.stringify({
+        jsonrpc: "2.0",
+        method: request.method,
+        params: request.params,
+        id: 0
+    });
+    const response = await fetch(url, requestInit);
+    const json = await response.json();
+    if ("error" in json) throw new Error(`${json.error.code}: ${json.error.message}`, { cause: { method: request.method, params: request.params, data: json.error.data } });
+    return json.result;
 }
 
-async function send(url:string, requests:Array<any>) {
+// send batched requests
+async function sendBatchedRequests(url:string, requestInit:RequestInit, requests:Array<{ method:string, params:any }>) {
     const _02_ = new Array(requests.length);
     for (let i = 0; i < requests.length; i++) {
-        _02_[i] = requests[i];
+        _02_[i] = {};
         _02_[i].jsonrpc = "2.0";
+        _02_[i].method = requests[i]!.method;
+        _02_[i].params = requests[i]!.params;
         _02_[i].id = i;
     }
-    const headers = { "Content-Type": "application/json" };
-    const body = JSON.stringify(requests.length == 1 ? _02_[0] : _02_);
-    const response = await fetch(url, { method: "POST", headers, body });
+    requestInit.body = JSON.stringify(_02_);
+    const response = await fetch(url, requestInit);
     const json = await response.json();
-    const errors = new Array<Error>();
-    if (_02_.length == 1) {
-            _4e4030_(errors, json);
-            _02_[0] = json.result;
-    } else {
-        for (let i = 0; i < _02_.length; i++) {
-            const request = json.find((_fc_:any) => _fc_.id == i);
-            if (request === undefined) {
-                const code = -32000;
-                const message = `response missing for request ${i}`;
-                const error = new Error(`${code}: ${message}`);
-                errors.push(error);
-                continue;
-            }
-            _4e4030_(errors, request);
-            _02_[i] = request.result;
-        }
+    json[1] = null;
+    const errors:Array<Error> = [];
+    const _54_ = new Array(requests.length);
+    const _a8_:Set<number> = new Set();
+    for (let i = 0; i < _02_.length; i++) {
+        const response = json.find((_fc_:any) => _fc_?.id == i);
+        if (response === undefined) { errors.push(new Error(`-32000: response missing for request ${i}`, { cause:{ method:_02_[i].method, params:_02_[i].params, id:_02_[i].id } })); _a8_.add(i); continue; }
+        if ("error" in response) { errors.push(new Error(`${response.error.code}: ${response.error.message}`, { cause:{ method:_02_[i].method, params:_02_[i].params, id:_02_[i].id, ...(response.error.data ? { data: response.error.data } : {}) } })) }
+        _54_[i] = response.result;
     }
-    if (errors.length) throw new AggregateError(errors);
-    return _02_;
+    if (errors.length) throw new AggregateError(errors, _a8_.size > 0 ? `-32000: responses missing for requests (${[..._a8_].join(' ')})`: undefined);
+    return _54_;
 }
 
-export { Client };
+async function send(url:string, ...requests:Array<{ method:string, params:any }>) {
+    const headers = { "Content-Type": "application/json" };
+    const requestInit = { method: "POST", headers };
+    if (requests.length == 1) return await sendSingleRequest(url, requestInit, requests[0]!);
+    else return await sendBatchedRequests(url, requestInit, requests);
+}
+
+export { Client, _fa5f71_ };
